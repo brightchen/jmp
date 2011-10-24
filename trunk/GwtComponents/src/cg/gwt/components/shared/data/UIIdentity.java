f7adf23c0af83dc5bbbc3945b9268bcf6fc9ceee
bright.chen.1 <bright.chen.1@d982c585-d153-a4a8-9abb-1f30da78d09f>
2011-10-24 20:36:23 +0000

    add concept Frame, which can be looked as page

--- trunk/GwtComponents/src/cg/gwt/components/shared/data/UIIdentity.java
+++ trunk/GwtComponents/src/cg/gwt/components/shared/data/UIIdentity.java
@@ -1,15 +1,24 @@
 package cg.gwt.components.shared.data;
 
+/**
+ * this enum includes information of UserManagement module. it should be refactored
+ * @author bright
+ *
+ */
 public enum UIIdentity
 {
   CONTROL_SECTION,        // ControlSectionUI
   UM_START,               //UserManagementUI
-  UM_CONTROL_PANEL,       //UserManagermentPanelUI
   UM_EMPTY,               //empty page;
+
+  UM_CONTROL_PANEL,       //UserManagermentPanelUI
+  
   UM_SEARCH_USER,
   UM_SEARCH_ACCOUNT,
   UM_ADD_ACCOUNT,
   UM_SEARCH_ROLE,
   UM_ADD_ROLE,
-  UM_ADD_PERMISSION
+  UM_ADD_PERMISSION,
+  
+  UM_LIST_USERS
 }
